fde3aa92bc62ee94f1873bb415056b6c427792d0
purple-cherries <19kaundc@tgsgirls.com>
2021-10-02 12:20:14 +0100

    Added Config link

--- config.js
+++ config.js
@@ -2,6 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+    apiKey: "AIzaSyDaaKGUt-uCLA-xTGKz5pq2bYTLzwIxg84",
+  authDomain: "team-voting-67d55.firebaseapp.com",
+  projectId: "team-voting-67d55",
+  storageBucket: "team-voting-67d55.appspot.com",
+  messagingSenderId: "122475262009",
+  appId: "1:122475262009:web:0f99601ba7f918ee219de8",
+  measurementId: "G-W37H2F80V1"
 
 };
 // Initialize Firebase
